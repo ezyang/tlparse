be38ac34a3f5fb2e2dbada1f029971ba2884a412
Edward Z. Yang <ezyang@meta.com>
2024-09-03 10:18:40 -0400

    Update
    [ghstack-poisoned]

--- src/templates.rs
+++ src/templates.rs
@@ -221,7 +221,7 @@ pub static TEMPLATE_COMPILATION_METRICS: &str = r#"
     <h2>Output files:</h2>
     <ul>
         {{ for path_idx in output_files }}
-            <li><a href="{compile_id_dir}/{path_idx.0}">{path_idx.1}</a> ({path_idx.2})</li>
+            <li><a href="{compile_id_dir}/{path_idx.filename}">{path_idx.display_filename}</a> ({path_idx.seq_nr}) {path_idx.size}</li>
         {{ endfor }}
     </ul>
     <h2>Stack</h2>
